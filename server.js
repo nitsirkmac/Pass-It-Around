b373d725d6ef6ee4eef9076f722ce61950905a3e
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-10-13 15:36:59 -0400

    Pass It Around complete

--- server.js
+++ server.js
@@ -2,9 +2,33 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Etheric Realm")
+    res.send(`
+    99 Bottles of beer on the wall <br>
+    99 bottles of beer! <br>
+    <a href="/98">Take one down, pass it around!</a>
+    `)
+    // link "take one down, pass it around"
+    // link to /98, 
 })
 
+app.get("/:number_of_bottles", (req, res) => {
+    numberOfBottles = req.params.number_of_bottles
+    if(numberOfBottles > 1) {
+        res.send(`
+        ${numberOfBottles} bottles of beer on the wall,<br>
+        ${numberOfBottles} bottles of beer.<br>
+        <a href="/${numberOfBottles - 1}">Take one down</a>, pass it around 
+        ${numberOfBottles - 1} bottles of beer on the wall
+        `)
+    } else if(numberOfBottles = 1) {
+        res.send(`
+        ${numberOfBottles} bottle of beer on the wall,<br>
+        ${numberOfBottles} bottle of beer.<br>
+        <a href="/">Maybe we should stop drinking?</a>
+        `)
+    }
+    
+})
 
 
 
